a71e842fae017e175658de6cfd896308fc13ae39
AvadhutShedge <shedgeavadhut2211@gmail.com>
2022-10-07 13:31:51 +0530

    no longer needed

--- school/admission/forms.py
+++
@@ -1,6 +0,0 @@
-from.models import contact_us
-from django import forms
-class contact_form(forms.ModelForm):
-    class Meta:
-        model = contact_us
-        fields = '__all__'
--- school/admission/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class contact_us(models.Model):
-    name=models.CharField(max_length=40)
-    email=models.EmailField()
-    phone=models.BigIntegerField(max_length=10)
-    message=models.TextField()
